546647738c4dd22365b01047c1bf13fd488d4fc8
m-khanddorj <m.khanddorj@gmail.com>
2019-06-17 10:40:44 +0800

    Updates

---
+++ precache-manifest.d73e01f9f6317ba4062b6b4daf6daa54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6439e04bdfb5d7d08f8ae37785032547",
+    "url": "/post-maker/index.html"
+  },
+  {
+    "revision": "4d39ae232fa934a78179",
+    "url": "/post-maker/static/css/main.60f1c0b8.chunk.css"
+  },
+  {
+    "revision": "63ed1646b2f605ba10aa",
+    "url": "/post-maker/static/js/2.1028f29a.chunk.js"
+  },
+  {
+    "revision": "4d39ae232fa934a78179",
+    "url": "/post-maker/static/js/main.1cb83fe5.chunk.js"
+  },
+  {
+    "revision": "3d702b54029ba09079e9",
+    "url": "/post-maker/static/js/runtime~main.6e4f6081.js"
+  },
+  {
+    "revision": "c2668daaa5bba5324bcb293a3cd47ca4",
+    "url": "/post-maker/static/media/1.c2668daa.png"
+  },
+  {
+    "revision": "a9b90011263fde6089e6a015dfaedf5f",
+    "url": "/post-maker/static/media/border.a9b90011.png"
+  }
+]);
